616f4dd39e34490eccaa47b06223322b663b092d
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-10-30 02:42:34 +0500

    Task3

---
+++ Tyuiu.ChetvertnyhAI.Sprint3.Task3.V10.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Specialized;
+using Tyuiu.ChetvertnyhAI.Sprint3.Task3.V10.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint3.Task3.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidDeleteCharInString()
+        {
+            DataService ds = new DataService();
+            string str = "gdfppf vfppt p";
+            char chr = 'p';
+
+            var res = ds.DeleteCharInString(str, chr);
+            
+            string wait = "gdff vft ";
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint3.Task3.V10/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint3.Task3.V10.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла foreach                                            *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #10                                                             *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Используя цикл foreach удалить из строки все буквы p                    *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        string value = "gdfppf vfppt p";
+        char chr = 'p';
+        Console.WriteLine("Исходная строчка" + value);
+        Console.WriteLine("Удаляемый символ" +  chr);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Полученная строка: " +ds.DeleteCharInString(value, chr));
+        Console.ReadKey();
+    }
+}

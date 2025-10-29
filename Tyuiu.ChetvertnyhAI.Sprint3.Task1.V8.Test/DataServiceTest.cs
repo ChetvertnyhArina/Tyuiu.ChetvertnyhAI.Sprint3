98500f15a98507b8c350b284a2a1eae88e10855a
ChetvertnyhArina <chetvertnyh-arina@mail.ru>
2025-10-29 23:32:33 +0500

    Task1

---
+++ Tyuiu.ChetvertnyhAI.Sprint3.Task1.V8.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint3.Task1.V8.Lib;
+namespace Tyuiu.ChetvertnyhAI.Sprint3.Task1.V8.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double value = 0.25;
+            int startValue = 1;
+            int stopValue = 7;
+
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+
+            double wait = -302185.684;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ChetvertnyhAI.Sprint3.Task1.V8/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.ChetvertnyhAI.Sprint3.Task1.V8.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнила: Четвертных А. И. | ИСТНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла while                                              *");
+        Console.WriteLine("* Задание #1                                                              *");
+        Console.WriteLine("* Вариант #8                                                              *");
+        Console.WriteLine("* Выполнила: Четвертных Арина Игоревна | ИСТНб-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл while                                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        DataService ds = new DataService();
+        double value = 0.25;
+        int startValue = 1;
+        int stopValue = 7;
+        Console.WriteLine("Переменная X = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Произведение = " + ds.GetSumSeries(value, startValue, stopValue));
+        Console.ReadKey(); ;
+    }
+}
